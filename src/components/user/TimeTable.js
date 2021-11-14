d6d6e7c5f6c6e713355adcbd820f2e823be3b2ce
이윤환 <zoo7930@sju.ac.kr>
2021-11-14 17:08:00 +0900

    Feature reservation time overlap process

--- src/components/user/TimeTable.js
+++ src/components/user/TimeTable.js
@@ -8,81 +8,128 @@ class TimeTable extends Component {
         super(props);
         this.canvasRef = React.createRef();
         this.ctx = null;
+        this.colorTable = ['cornflowerblue', 'mediumblue', 'steelblue', 'darkslateblue', 'darkblue', 'blueviolet', 'mediumvioletred']
         this.state = {
             width: '100%',
-            height: '730px'
+            height: '730px',
+            timeData: this.props.reservationTimeList,
+            timeCoordinate: []
+
         }
     }
     componentDidMount() {
-        const timeData = [
-            { startTime: '10:00', endTime: '10:30' }, { startTime: '11:00', endTime: '11:20' },
-            { startTime: '11:20', endTime: '11:40' }, { startTime: '12:00', endTime: '12:20' },
-            { startTime: '14:20', endTime: '14:40' }, { startTime: '14:40', endTime: '15:20' },
-            { startTime: '17:00', endTime: '17:20' }, { startTime: '17:30', endTime: '17:45' },
-            { startTime: '18:00', endTime: '18:20' }, { startTime: '20:00', endTime: '20:25' }
-        ]
 
-        const colorTable = ['cornflowerblue', 'mediumblue', 'steelblue', 'darkslateblue', 'darkblue', 'blueviolet', 'mediumvioletred']
-
-        const canvas = document.querySelector("canvas");
-        canvas.width = window.innerWidth - 30;
+        //캔버스 설정(DOM 접근은 compnentDidMount 에서만 접근할 수 있으므로 여기서 캔버스를 설정해 준다.)
+        this.canvas = document.querySelector("canvas");
+        this.canvas.width = window.innerWidth - 30;
         this.ctx = this.canvasRef.current.getContext("2d");
-        this.ctx.fillStyle = 'green';
-        //this.ctx.fillRect(10, 10, 250, 100);
         this.ctx.fillStyle = 'black';
         this.ctx.font = '14px 나눔스퀘어';
 
+        //테이블 바탕 출력
+        this.drawBaseTimeTable();
 
-        var time = [];
-        for (let i = 0; i < 24; i += 2) {
-            time.push((i / 10 < 1 ? '0' + i : i) + ":00");
-        }
+        this.currentSelectedTimeIndex = 0;
 
-        this.ctx.fillStyle = 'black';
-        for (let i = 0; i < 12; i++) {
-            this.ctx.fillText(time[i], 0, i * 60 + 20);
-        }
+        //초기 그래프 그리기
+        this.state.timeData.forEach((value, index) => {
+            //운동시작시간에 해당하는 좌표와 종료시간의 좌표로 높이 구하기
+            let y = parseInt(this.getYCoordinate(value.startTime));
+            let height = parseInt(this.getYCoordinate(value.endTime) - y);
 
-        this.ctx.fillStyle = '#A0A0A0';
-        for (let i = 0; i < 12; i++) {
-            this.ctx.fillRect(50, i * 60 + 13, 600, 1);
-        }
-
-        this.ctx.fillStyle = 'orange';
-        timeData.forEach((value, index) => {
-            let start = this.getYCoordinate(value.startTime);
-            let end = this.getYCoordinate(value.endTime);
-
-            let y = (start * 60 + 13) / 2;
-            let height = (end * 60 + 13) / 2 - y;
-
-            this.ctx.fillStyle = colorTable[index % timeData.length];
+            this.state.timeCoordinate.push({ startY: y, endY: y + height });
+            this.ctx.fillStyle = this.colorTable[index % this.colorTable.length];
 
             this.ctx.fillRect(50, y, 600, height);
 
-            if (index === 5) {
-                this.ctx.fillRect(40, y, 600, height);
-                this.ctx.fillStyle = 'orange';
-
-            }
+            // if (index === this.currentSelectedTimeIndex) {
+            //     this.ctx.fillRect(40, y, 600, height);
+            // }
 
         })
 
     }
+
+
+    onClickCanvas(e) {
+        var rect = this.canvas.getBoundingClientRect();
+        var x = parseInt(e.clientX - rect.left);
+        var y = parseInt(e.clientY - rect.top);
+        console.log(this.getTimeIndex(y));
+
+        const timeIndex = this.getTimeIndex(y);
+
+        if (timeIndex >= 0) this.props.selectTime({ time: this.state.timeData[timeIndex], color: this.colorTable[timeIndex % this.colorTable.length] });
+
+        //바가 클릭 됐을 때
+        if (timeIndex >= 0 && x > 50) {
+            let height = this.state.timeCoordinate[timeIndex].endY - this.state.timeCoordinate[timeIndex].startY;
+
+            let colorIndex = timeIndex % this.colorTable.length;
+            this.ctx.fillStyle = this.colorTable[colorIndex];
+            this.ctx.fillRect(40, this.state.timeCoordinate[timeIndex].startY, 600, height);
+
+            if (timeIndex !== this.currentSelectedTimeIndex) {
+                let previousTimeHeight = this.state.timeCoordinate[this.currentSelectedTimeIndex].endY - this.state.timeCoordinate[this.currentSelectedTimeIndex].startY;
+
+                this.ctx.fillStyle = 'white';
+                this.ctx.fillRect(40, this.state.timeCoordinate[this.currentSelectedTimeIndex].startY, 600, previousTimeHeight);
+
+                this.ctx.fillStyle = this.colorTable[this.currentSelectedTimeIndex % this.colorTable.length];
+                this.ctx.fillRect(50, this.state.timeCoordinate[this.currentSelectedTimeIndex].startY, 600, previousTimeHeight);
+
+                this.currentSelectedTimeIndex = timeIndex;
+            }
+
+        }
+    }
+
 
     getYCoordinate(time) {
         time = time.split(':');
         let hour = parseInt(time[0]);
         let minute = parseInt(time[1]);
 
-        return (hour + minute / 60)
+        return ((hour + minute / 60) * 60) / 2 + 13;
+    }
+
+    getTimeIndex(y) {
+
+        let timeIndex = -1;
+
+        this.state.timeCoordinate.forEach((time, index) => {
+            if (time.startY <= y && time.endY >= y) {
+                timeIndex = index;
+                return;
+            }
+        })
+
+        return timeIndex;
+    }
+
+    drawBaseTimeTable() {
+        let baseTime = [];
+
+        for (let i = 0; i < 24; i += 2) {
+            baseTime.push((i / 10 < 1 ? '0' + i : i) + ":00");
+        }
+
+        this.ctx.fillStyle = 'black';
+        for (let i = 0; i < 12; i++) {
+            this.ctx.fillText(baseTime[i], 0, i * 60 + 20);
+        }
+
+        this.ctx.fillStyle = '#A0A0A0';
+        for (let i = 0; i < 12; i++) {
+            this.ctx.fillRect(50, i * 60 + 13, 600, 1);
+        }
     }
 
     render() {
-
         return (
             <TimeTableStyle>
-                <canvas ref={this.canvasRef} width={this.state.width} height={this.state.height} />
+                <canvas ref={this.canvasRef} width={this.state.width} height={this.state.height}
+                    onClick={this.onClickCanvas.bind(this)} />
             </TimeTableStyle>
         );
     }
--- src/pages/user/Reservation/TimeSelection.js
+++ src/pages/user/Reservation/TimeSelection.js
@@ -3,63 +3,157 @@ import TopBar from '../../../components/user/TopBar';
 import Modal from '../../../components/user/Modal';
 import styled from 'styled-components';
 import TimeTable from '../../../components/user/TimeTable';
-
+import InputButton from '../../../components/user/InputButton';
 class TimeSelectionModal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            //timeTable에서 선택하여 조회한 시간
+            selectedStartTime: '',
+            selectedEndTime: '',
+            selectedBarColor: 'grey',
+
+            //예약하고자하는 시간
+            startTimeHour: '0',
+            startTimeMinute: '0',
+            exerciseMinute: '10',
+
+            //영업시간
+            openTime: '06:00',
+            closeTime: '2:00',
+
+            //예약가능시간 체크
+            isValid: false,
+            guideMessage: ' ',
+
+            reservationTimeList: [
+                { startTime: '10:00', endTime: '10:30' }, { startTime: '11:00', endTime: '11:20' },
+                { startTime: '11:20', endTime: '11:40' }, { startTime: '12:00', endTime: '12:20' },
+                { startTime: '14:20', endTime: '14:40' }, { startTime: '14:40', endTime: '15:20' },
+                { startTime: '17:00', endTime: '17:20' }, { startTime: '17:30', endTime: '17:45' },
+                { startTime: '18:00', endTime: '18:20' }, { startTime: '20:00', endTime: '20:25' }
+            ]
+        }
+    }
+
+    getSelectedBarTime(data) {
+        this.setState({
+            selectedStartTime: data.time.startTime,
+            selectedEndTime: data.time.endTime,
+            selectedBarColor: data.color
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        //서버에 요청보내기
+    }
+
+    checkOverlap() {
+        let start = this.state.startTimeHour + ":" + this.state.startTimeMinute;
+        let exercieMinute = this.state.exerciseMinute;
+        let startTime = this.changeTimeNumber(start);
+        let endTime = startTime + exercieMinute / 60;
+
+        for (let index = 0; index < this.state.reservationTimeList.length; index++) {
+            let time = this.state.reservationTimeList[index];
+
+            let reservedStartTime = this.changeTimeNumber(time.startTime);
+            let reservedEndTime = this.changeTimeNumber(time.endTime);
+
+
+            //겹치는 시간 발생
+            console.log(startTime);
+            console.log(endTime);
+
+            if (endTime > reservedStartTime && startTime < reservedEndTime) {
+                console.log("asdf")
+
+                this.setState({
+                    //예약가능시간 체크
+                    isValid: false,
+                    guideMessage: '다른 사용자가 이미 예약한 시간입니다.'
+                })
+
+                return;
+            }
+        }
+        this.setState({
+            //예약가능시간 체크
+            isValid: true,
+            guideMessage: '예약 가능한 시간입니다.'
+        });
+    }
+
+
+    changeTimeNumber(time) {
+        let timeArr = time.split(':');
+        return parseInt(timeArr[0]) + parseInt(timeArr[1]) / 60
+    }
+
+
+
+    handleChange(e, propertyName) {
+        console.log(e.target.value)
+        this.setState({ [propertyName]: e.target.value }, () => {
+            this.checkOverlap();
+        });
+
+    }
+
+
+
     render() {
-        // var time = [];
-        // for (let i = 0; i < 24; i++) {
-        //     time.push((i / 10 < 1 ? '0' + i : i) + ":00");
-        // }
-        // console.log(time);
 
-        // const timeList = time.map(time => <TR>{time}</TR>);
-        // const timeList2 = time.map(time => <TR><Trgraph isUsing={time.includes('1')}></Trgraph></TR>);
+        let startHour = Array.from({ length: 24 }, (v, i) => i);
+        let startHourOptions = startHour.map((value, index) => {
+            return <OptionStyle value={value} key={value}>{value}</OptionStyle>
+        })
+
+        let StartMinute = Array.from({ length: 12 }, (v, i) => i * 5);
+        let StartMinuteOptions = StartMinute.map((value, index) => {
+            return <OptionStyle value={value} key={value} > {value}</OptionStyle >
+        })
+
+        let exerciseMinute = Array.from({ length: 7 }, (v, i) => i * 5 + 10);
+        let exerciseMinuteOptions = exerciseMinute.map((value, index) => {
+            return <OptionStyle value={value} key={value} > {value}</OptionStyle >
+        })
+
+
         return (
-
-
-            <Modal onClick={this.props.closeModal}>
+            <Modal onClick={this.props.closeModal} >
                 {this.props.children}
-                <br />
-                <TimeTable></TimeTable>
-                {/* <StyledTimeGraph>
-                    <table>
-                        <td>
-                            {timeList}
-                        </td>
-                        <td>
-                            {timeList2}
-                        </td>
-                    </table>
-                </StyledTimeGraph> */}
-                <MessageStyle>
-                    14:40 - 15:20
+                < br />
+                <TimeTable selectTime={this.getSelectedBarTime.bind(this)} reservationTimeList={this.state.reservationTimeList}></TimeTable>
+                <MessageStyle barColor={this.state.selectedBarColor}>
+                    {this.state.selectedStartTime} -  {this.state.selectedEndTime}
                 </MessageStyle>
 
-                <StyledStartTime>
-                    <SelectStyle name="시" id="hour">
-                        <OptionStyle value="0">10</OptionStyle>
-                    </SelectStyle>
-                    시
-                    <SelectStyle name="시" id="hour">
-                        <OptionStyle value="0">30</OptionStyle>
-                    </SelectStyle>
-                    분 부터
-                </StyledStartTime>
-                <StyledEndTime>
-                    <SelectStyle name="시" id="hour">
-                        <OptionStyle value="0">11</OptionStyle>
-                    </SelectStyle>
-                    시
-                    <SelectStyle name="시" id="hour">
-                        <OptionStyle value="0">00</OptionStyle>
-                    </SelectStyle>
-                    분 까지
-                </StyledEndTime>
-                <StyledButtonArea>
-                    <StyledMenuText>
-                        예약
-                    </StyledMenuText>
-                </StyledButtonArea>
+                <form onSubmit={this.handleSubmit.bind(this)} autoComplete={"off"}>
+                    <ReservationTimeStyle>
+                        <SelectStyle name="시" id="startHour" value={this.state.startTimeHour}
+                            onChange={(e) => this.handleChange(e, 'startTimeHour')}>
+                            {startHourOptions}
+                        </SelectStyle>
+                        시
+                        <SelectStyle name="분" id="startMinute" value={this.state.startTimeMinute}
+                            onChange={(e) => this.handleChange(e, 'startTimeMinute')}>
+                            {StartMinuteOptions}
+                        </SelectStyle>
+                        분 부터
+                        <SelectStyle name="분" id="minute" value={this.state.exerciseMinute}
+                            onChange={(e) => this.handleChange(e, 'exerciseMinute')}>
+                            {exerciseMinuteOptions}
+                        </SelectStyle>
+                        분 운동
+                    </ReservationTimeStyle>
+                    {this.state.isValid ? <ValidMessageStyle>{this.state.guideMessage} </ValidMessageStyle> : <ErrorMessageStyle>{this.state.guideMessage}</ErrorMessageStyle>}
+                    <InputButton type='submit' value='예약'></InputButton>
+                </form>
             </Modal>
         );
     }
@@ -71,11 +165,10 @@ var MessageStyle = styled.div`
     width:200px;
     font-size:20px;
     border-radius:4px;
-    background-color: blueviolet;
+    background-color: ${props => props.barColor};
     color:white;
     margin-top:20px;
     margin-bottom:20px;
-
 `;
 
 var SelectStyle = styled.select`
@@ -83,73 +176,41 @@ var SelectStyle = styled.select`
     background: url('arrow.jpg') no-repeat 95% 50%;
     font-size:40px;
 
-`;
-
-let StyledStartTime = styled.div`
-padding-left:40px;
-text-align:left;
-height: 60px;
-width: 100%;
-`;
-
-let StyledEndTime = styled.div`
-padding-right:40px;
-text-align:right;
-height: 60px;
-width: 100%;
-`;
-
-var OptionStyle = styled.option`
-
-`;
-
-var StyledMenuText = styled.div`
-    font-size:15px;
-    display: inline-block;
-    vertical-align:middle;
-    text-align: center;
-
-`;
-
-
-var StyledButtonArea = styled.div`
-    height:50px;
-    width:99%;
-    max-width:500px;
-    margin: 0 auto;
-    padding-top:10px;
-    padding-bottom:10px;
-    margin-top:10px;
-    background-color:#404040;
-    color:white;
-    border-radius:6px;
-`
-
-let StyledTimeGraph = styled.div`
-overflow:scroll;
-height:400px;
-list-style:none;
-width: 100%;
+    list-style:none;
 white-space:nowrap;
     background-color:rgba(255,255,255,0);
     ::-webkit-scrollbar{
         display:none;
     }
 `;
+var OptionStyle = styled.option`
+    font-size:20px;
 
-var TR = styled.tr`
-    height:50px;
-    font-size:13px;
 `;
 
-var Trgraph = styled.div`
-    display:static;
-    height:50px;
-    width:300px;
-    font-size:13px;
-    background-color:${props => props.isUsing ? '#909090' : '#FFFFFF'}
+let ReservationTimeStyle = styled.div`
+text-align:left;
+height: 60px;
+width: 100%;
+border:none;
 `;
 
+let ValidMessageStyle = styled.div`
+    padding:10px;
+    color:green;
+    font-size:16px;
+    margin: 0 auto;
+    width:100%;
+`;
+
+let ErrorMessageStyle = styled.div`
+    padding:10px;
+    
+    color:red;
+    font-size:16px;
+    margin: 0 auto;
+    width:100%;
+`;
 
 
 export default TimeSelectionModal;
